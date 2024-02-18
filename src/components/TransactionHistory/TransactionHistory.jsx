1e2b12fda2343f8a62021c1466a805ec7692042a
Yana <yanalunina96@gmail.com>
2024-02-18 13:43:18 +0200

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -15,8 +15,8 @@ const TransactionHistory = ({ transactions }) => {
         {transactions.map(({ id, type, amount, currency }) => {
           return (
             <tr key={id} className={css.tr}>
-              <td className={css.td}>{amount}</td>
               <td className={css.td}>{type}</td>
+              <td className={css.td}>{amount}</td>
               <td className={css.td}>{currency}</td>
             </tr>
           );
